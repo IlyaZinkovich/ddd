c79377217f679d81d7804b40aa72b9e6b151b8b9
Ilya Zinkovich <ilya.zinkovich@gmail.com>
2017-12-16 15:54:36 +0300

    Added spring context test

---
+++ src/test/java/com/ddd/ask/application/AskApplicationSpringContextTest.java
@@ -0,0 +1,11 @@
+package com.ddd.ask.application;
+
+import org.junit.Test;
+
+public class AskApplicationSpringContextTest {
+
+    @Test
+    public void springContextTest() {
+        AskApplication.main(new String[]{});
+    }
+}
